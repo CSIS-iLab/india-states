1aef6b7f5d12bed9675639203dcd57fc010aba9a
Jacque Schrag <jnschrag@gmail.com>
2017-02-17 16:51:27 -0500

    Added subsector filtering to sector pages

--- assets/js/post-listing.js
+++ assets/js/post-listing.js
@@ -2,12 +2,25 @@ $(function() {
 	/*==============================================
 	=            Post Listing Filtering            =
 	==============================================*/
+	// Get and Set Active Filter if we have one in the URL
+	var activeSubsector = window.location.hash.replace("#", "");
+	var path = window.location.pathname.split('/');
+	var activeSector = path[path.length - 1];
+	console.log(activeSector);
+	if(activeSubsector) {
+		switchActiveSubsector(activeSector, activeSubsector); // Switch active subsector to show those posts
+	}
+
 	$(".subsector-link").click(function() {
 		var activeSector = $(this).data("sector"); // Get active sector
 		var activeSubsector = $(this).data("show-subsector"); // Get active subsector
+		switchActiveSubsector(activeSector, activeSubsector); // Switch active subsector to show those posts
+	});
+
+	function switchActiveSubsector(activeSector, activeSubsector) {
 		$(".post-listing[data-sector='"+activeSector+"'] .active").removeClass("active").addClass("hidden");
 		$(".post-listing[data-sector='"+activeSector+"'] [data-subsector='"+activeSubsector+"']").removeClass("hidden").addClass("active");
-	});
+	}
 	
 	
 	/*=====  End of Post Listing Filtering  ======*/
