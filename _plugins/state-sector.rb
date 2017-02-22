d2db7cea94826b4fed06d7530dcd3ba501fc321d
Jacque Schrag <jnschrag@gmail.com>
2017-02-22 15:46:54 -0500

    Create State-Sector Archive Generator
    Created a custom Jekyll plugin that creates an individual archive page
    for each state and all sectors. For example, for the state of “Assam”,
    there is now an archive page for “Assam Energy” and “Assam Health”. The
    generator runs on the build process, so any time new states or sectors
    are created, they will be added.

---
+++ _plugins/state-sector.rb
@@ -0,0 +1,41 @@
+require "jekyll"
+
+module Jekyll
+
+  class ArchivePage < Page
+    def initialize(site, base, dir, filename, state, sector)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = filename+'.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'state-sector-archive.html')
+      self.data['title'] = filename
+      self.data['state'] = state
+      self.data['sector'] = sector
+
+    end
+  end
+
+  class ArchivePageGenerator < Jekyll::Generator
+    safe true
+
+    def generate(site)
+      # p site.collections['states']
+      if site.layouts.key? 'state-sector-archive'
+        dir = 'states'
+        # Loop through states
+        site.collections['states'].docs.each do |state|
+          # Loop through categories and create a new archive page for each state-sector combo
+          site.collections['sectors'].docs.each do |sector|
+            filename = state.data["slug"] + "-" + sector.data["slug"]
+            site.pages << ArchivePage.new(site, site.source, File.join(dir), filename, state.data["title"], sector.data["title"])
+          end
+
+        end
+      end
+    end
+  end
+
+end
