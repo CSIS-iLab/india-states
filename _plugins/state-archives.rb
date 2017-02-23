27976eced267a1a95acbec887e46f1ae9ceea8ee
Jacque Schrag <jnschrag@gmail.com>
2017-02-23 18:07:25 -0500

    Created a separate archive template for state-archive pages
    /state/archive gets a different template from /state/sector because
    there is no subsector filtering. Actual template to be designed

--- _plugins/state-archives.rb
+++ _plugins/state-archives.rb
@@ -1,5 +1,6 @@
 require "jekyll"
 
+# Creates /state/sector/ pages
 module Jekyll
 
   class ArchivePage < Page
@@ -7,10 +8,17 @@ module Jekyll
       @site = site
       @base = base
       @dir = dir
-      @name = filename+'.html'
+      @name = 'index.html'
+
+      if sector == 'archive'
+        @layout = 'state-archive'
+      else
+        @layout = 'state-sector-archive'
+      end
+
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'state-sector-archive.html')
+      self.read_yaml(File.join(base, '_layouts'), @layout+'.html')
       self.data['title'] = filename
       self.data['state'] = state
       self.data['sector'] = sector
@@ -29,12 +37,12 @@ module Jekyll
           # Loop through categories and create a new archive page for each state-sector combo
           site.collections['sectors'].docs.each do |sector|
             filename = state.data["slug"] + "-" + sector.data["slug"]
-            site.pages << ArchivePage.new(site, site.source, File.join(dir), filename, state.data["title"], sector.data["title"])
+            site.pages << ArchivePage.new(site, site.source, File.join(dir, state.data["slug"], sector.data["slug"]), filename, state.data["title"], sector.data["title"])
           end
 
           # Create an all archive
-          filename = state.data["slug"] + "-all"
-          site.pages << ArchivePage.new(site, site.source, File.join(dir), filename, state.data["title"], "all")
+          filename = state.data["slug"] + "-archive"
+          site.pages << ArchivePage.new(site, site.source, File.join(dir, state.data["slug"], "archive"), filename, state.data["title"], "archive")
 
         end
       end
