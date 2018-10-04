39e426627949bcd00d25afdb4137487bdf404b18
leezaweb <leeza.web@gmail.com>
2018-10-04 13:09:19 -0400

    progress bar

--- assets/js/national-goals.js
+++ assets/js/national-goals.js
@@ -1,36 +1,41 @@
 $(function() {
-    // on page load...
+  // on page load...
+  moveProgressBar();
+  // on browser resize...
+  $(window).resize(function() {
     moveProgressBar();
-    // on browser resize...
-    $(window).resize(function() {
-        moveProgressBar();
+  });
+
+  // SIGNATURE PROGRESS
+  function moveProgressBar() {
+    $(".progress-wrap").each(function() {
+      var getPercent = $(this).data("progress-percent") / 100;
+      var getProgressWrapWidth = $(this).width();
+      var progressTotal = getPercent * getProgressWrapWidth;
+      var animationLength = 2500;
+
+      // on page load, animate percentage bar to data percentage length
+      // .stop() used to prevent animation queueing
+      $(this)
+        .children(".progress-bar")
+        .stop()
+        .animate(
+          {
+            width: progressTotal - 10
+          },
+          animationLength
+        );
     });
+  }
 
-    // SIGNATURE PROGRESS
-    function moveProgressBar() {
-        $('.progress-wrap').each(function() {
-            var getPercent = ($(this).data('progress-percent') / 100);
-            var getProgressWrapWidth = $(this).width();
-            var progressTotal = getPercent * getProgressWrapWidth;
-            var animationLength = 2500;
+  var tooltips = document.querySelectorAll(".tip-content");
 
-            // on page load, animate percentage bar to data percentage length
-            // .stop() used to prevent animation queueing
-            $(this).children('.progress-bar').stop().animate({
-                width: progressTotal
-            }, animationLength);
-        });
+  window.onmousemove = function(e) {
+    var x = e.clientX + 20 + "px",
+      y = e.clientY + 20 + "px";
+    for (var i = 0; i < tooltips.length; i++) {
+      tooltips[i].style.top = y;
+      tooltips[i].style.left = x;
     }
-
-    var tooltips = document.querySelectorAll('.tip-content');
-
-    window.onmousemove = function (e) {
-        var x = (e.clientX + 20) + 'px',
-            y = (e.clientY + 20) + 'px';
-        for (var i = 0; i < tooltips.length; i++) {
-            tooltips[i].style.top = y;
-            tooltips[i].style.left = x;
-        }
-    };
-
+  };
 });
