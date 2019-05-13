ed4b73b49c9aa129b42ccec5f201bf12dc2bb4e1
leezaweb <leeza.web@gmail.com>
2019-05-13 13:25:00 -0400

    fix states sticky

--- assets/js/sticky-menu.js
+++ assets/js/sticky-menu.js
@@ -1,67 +1,71 @@
 $(function() {
-	if($(".isSticky").length) {
-		var windw = this;
-		var headerHeight = $(".site-header").outerHeight() + 32;
-		var followToPos = $(".isSticky").offset().top - headerHeight;
-		var tocPadding = $(".isSticky").parent(".tableOfContents").offset().top - $(".isSticky").offset().top;
-		var stopFollowPos = $(".isSticky").parent(".tableOfContents").outerHeight();
-		var stickyWidth = $(".isSticky").width();
+  if ($(".isSticky").length) {
+    var windw = this;
 
-		$.fn.followTo = function ( pos ) {
-		    var $this = this,
-		        $window = $(windw);
-		    
-		    $window.scroll(function(e){
-		    	currentPos = $(document).height() - ($(window).height() + $('body').scrollTop());
-		        if ($window.scrollTop() > pos && currentPos < stopFollowPos) {
-		            $this.css({
-		                position: 'fixed',
-		                top: (headerHeight),
-		                width: stickyWidth
-		            });
-		        } 
-		        else if ($window.scrollTop() < pos) {
-		            $this.css({
-		                position: 'initial',
-		                top: 0
-		            });
-		        }
-		        // else if (currentPos > 1640) {
-		        // 	$this.css({
-		        //         position: 'absolute',
-		        //         bottom: 0
-		        //     });
-		        // }
-		    });
-		};
+    var headerHeight = $(".site-header").outerHeight() + 32;
 
-		$('.isSticky').followTo(followToPos);
+    $.fn.followTo = function() {
+      var $this = this;
 
-		
+      var stickyWidth = $this.width();
 
+      var thisBox = $this[0].getBoundingClientRect();
+      var tocBox = $this.parent(".tableOfContents")[0].getBoundingClientRect();
 
+      var tocTop = tocBox.top + window.scrollY - headerHeight;
+      var tocBottom = tocBox.bottom + window.scrollY - headerHeight;
 
-		// Increase body padding if we have a fixedMenu
-		// if($('.tableOfContents.fixedMobile').length) {
-		// 	var tocHeight = $('.tableOfContents.fixedMobile').outerHeight();
-		// 	var newPadding = tocHeight + headerHeight;
-		// 	$("body").css("padding-top",newPadding+"px");
-		// 	$(".tableOfContents.fixedMobile").css("top",headerHeight+"px");
-		// }
-	}
+      if (window.scrollY > tocTop && window.scrollY < tocBottom) {
+        $this.css({
+          position: "fixed",
+          top: headerHeight,
+          width: stickyWidth
+        });
+      }
 
-	// Drop down menu for mobile
-	if($(".tableOfContents-dropdown").length) {
-		$('.tableOfContents-dropdown').on('change', function () {
-	        var url = $(this).val(); // get selected value
-			window.location = url; // redirect
+      var bottom = thisBox.bottom + window.scrollY;
+      var top = tocBox.bottom + window.scrollY - headerHeight - $this.height();
 
-			if(url != "#") {
-				$(".tableOfContents-instructions").css("display","inline-block");
-			}
-			else {
-				$(".tableOfContents-instructions").hide();
-			}
-	    });
-	}
+      $(window).scroll(function(e) {
+        if (window.scrollY > tocTop && window.scrollY < bottom) {
+          $this.css({
+            position: "fixed",
+            top: headerHeight,
+            width: stickyWidth
+          });
+        } else if (window.scrollY < tocTop || window.scrollY > top) {
+          $this.css({
+            position: "initial",
+            top: 0
+          });
+        }
+      });
+    };
+
+    $(".isSticky").each(function() {
+      $(this).followTo();
+    });
+
+    // Increase body padding if we have a fixedMenu
+    // if($('.tableOfContents.fixedMobile').length) {
+    // 	var tocHeight = $('.tableOfContents.fixedMobile').outerHeight();
+    // 	var newPadding = tocHeight + headerHeight;
+    // 	$("body").css("padding-top",newPadding+"px");
+    // 	$(".tableOfContents.fixedMobile").css("top",headerHeight+"px");
+    // }
+  }
+
+  // Drop down menu for mobile
+  if ($(".tableOfContents-dropdown").length) {
+    $(".tableOfContents-dropdown").on("change", function() {
+      var url = $(this).val(); // get selected value
+      window.location = url; // redirect
+
+      if (url != "#") {
+        $(".tableOfContents-instructions").css("display", "inline-block");
+      } else {
+        $(".tableOfContents-instructions").hide();
+      }
+    });
+  }
 });
